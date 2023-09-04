4fbd3d3b05e92be8a436a57a93815d4d9ca4c4b9
CBarba12 <alexbarba1994@hotmail.com>
2023-09-04 01:24:03 -0600

    mvd

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -2,20 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyecto_definitivo_progra_3;
+package Controlador;
+
+import Vista.VistaPrincipal;
 
 /**
  *
  * @author alexb
  */
-public class Proyecto_definitivo_progra_3 {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Diseño_2 p=new Diseño_2();
+        VistaPrincipal p=new VistaPrincipal();
         p.setVisible(true);
         
     }
